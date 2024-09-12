2ad7082f1720a87a92739d229c75dd52ff49744f
61milenap <61milenap8678@gmail.com>
2024-09-12 12:39:04 +0300

    menu2

--- lab_Postnova/lab_Postnova/lab_Postnova.cpp
+++ lab_Postnova/lab_Postnova/lab_Postnova.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+using namespace std;
 int main()
 {
-    std::cout << "Μενώ:\n";
+    cout << "Menu:\n";
+    cout << "1. Add pipes;\n";
+    cout << "2. Add KC;\n";
+    cout << "3. All objects;\n";
+    cout << "4. Edit pipes;\n";
+    cout << "5. Edit KC;\n";
+    cout << "6. Save;\n";
+    cout << "7. Download;\n";
+    cout << "0. Exit\n";
+    cout << "Enter the number\n";
+    int number;
+    cin >> number;
+    
+
+
 }
 
