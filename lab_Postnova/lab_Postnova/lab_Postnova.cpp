3e9625878463b65e8077f60fa5430032535b7af9
61milenap <61milenap8678@gmail.com>
2024-09-12 18:50:01 +0300

    command while-do

--- lab_Postnova/lab_Postnova/lab_Postnova.cpp
+++ lab_Postnova/lab_Postnova/lab_Postnova.cpp
@@ -5,38 +5,38 @@ int main()
 {
     //меню приложения
     cout << "Menu:\n";
-    cout << "1. Add pipes;\n";
-    cout << "2. Add KC;\n";
-    cout << "3. All objects;\n";
-    cout << "4. Edit pipes;\n";
-    cout << "5. Edit KC;\n";
-    cout << "6. Save;\n";
-    cout << "7. Download;\n";
-    cout << "0. Exit\n";
+    cout << "1. Add pipes;\n" << "2. Add KC;\n" << "3. All objects;\n" << "4. Edit pipes;\n"
+        << "5. Edit KC;\n" << "6. Save;\n" << "7. Download;\n" << "0. Exit\n";
     cout << "Enter the number\n";
     int number;
+    const int max{ 2 };
+    char objects[max];
+
     do {
         cin >> number;
         if (number == 1) {
-
+            cout << "Command 1. Add pipes\n";
         }
         else if (number == 2) {
-
+            cout << "Command 2. Add KC\n";
         }
         else if (number == 3) {
-
+            cout << "Command 3. All objects\n";
         }
         else if (number == 4) {
-
+            cout << "Command 4. Edit pipes\n";
         }
         else if (number == 5) {
-
+            cout << "Command 5. Edit KC\n";
         }
         else if (number == 6) {
-
+            cout << "Command 6. Save\n";
         }
         else if (number == 7) {
-
+            cout << "Command 7. Download\n";
+        }
+        else if (number >= 8) {
+            cout << "Enter another command number\n";
         }
     } while (number != 0);
     cout << "End\n";
