0a793369c00c3d2d6ea5e7c70879c60956dd5d68
61milenap <61milenap8678@gmail.com>
2024-09-12 19:18:08 +0300

    new list append elements

--- lab_Postnova/lab_Postnova/lab_Postnova.cpp
+++ lab_Postnova/lab_Postnova/lab_Postnova.cpp
@@ -6,35 +6,44 @@ int main()
 {
     //меню приложения
     cout << "Menu:\n";
-    cout << "1. Add pipes;\n" << "2. Add KC;\n" << "3. All objects;\n" << "4. Edit pipes;\n"
-        << "5. Edit KC;\n" << "6. Save;\n" << "7. Download;\n" << "0. Exit\n";
+    cout << "1. Add pipes;\n" << "2. Add KC;\n" 
+        << "3. All objects;\n" << "4. Edit pipes;\n"
+        << "5. Edit KC;\n" << "6. Save;\n" 
+        << "7. Download;\n" << "0. Exit\n";
     cout << "Enter the number\n";
     int number;
     list<string>objects{};
     do {
         cin >> number;
         if (number == 1) {
-            cout << "Command 1. Add pipes\n";
-            objects.push_back("Pipes");
+            cout << "Add pipes\n";
+            objects.push_back( "Pipes" );
         }
         else if (number == 2) {
-            cout << "Command 2. Add KC\n";
-            objects.push_back("KC");
+            cout << "Add KC\n";
+            objects.push_back( "KC" );
         }
         else if (number == 3) {
-            cout << "Command 3. All objects\n";
+            cout << "All objects\n";
+            if (objects.size() != 0) {
+                for (string n : objects)
+                    std::cout << n << "\n";
+            }
+            else {
+                cout << "No objects\n";
+            }
         }
         else if (number == 4) {
-            cout << "Command 4. Edit pipes\n";
+            cout << "Edit pipes\n";
         }
         else if (number == 5) {
-            cout << "Command 5. Edit KC\n";
+            cout << "Edit KC\n";
         }
         else if (number == 6) {
-            cout << "Command 6. Save\n";
+            cout << "Save\n";
         }
         else if (number == 7) {
-            cout << "Command 7. Download\n";
+            cout << "Download\n";
         }
         else if (number >= 8) {
             cout << "Enter another command number\n";
