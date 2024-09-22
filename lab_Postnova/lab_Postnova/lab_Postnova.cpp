61b105b8d379011fca72292e3349e668b3db95a6
61milenap <61milenap8678@gmail.com>
2024-09-22 16:51:36 +0300

    new switch case

--- lab_Postnova/lab_Postnova/lab_Postnova.cpp
+++ lab_Postnova/lab_Postnova/lab_Postnova.cpp
@@ -3,13 +3,12 @@
 
 using namespace std;
 
-
 struct Pipe {
-    int mark_kilometr, length, diameter;
-    char repair;
+    char mark_kilometr;
+    double length = 0.0, diameter = 0.0, repair = 0.0;
 };
 
-struct PC {
+struct KS {
     string name;
     int count_shop, count_workshop;
     char perfomance;
@@ -17,61 +16,45 @@ struct PC {
 
 int main()
 {
+    int number;
     //меню приложения
     cout << "Menu:\n";
     cout << "1. Add pipes;\n" << "2. Add KC;\n" 
-        << "3. All objects;\n" << "4. Edit pipes;\n"
-        << "5. Edit KC;\n" << "6. Save;\n" 
-        << "7. Download;\n" << "0. Exit\n";
+         << "3. All objects;\n" << "4. Edit pipes;\n"
+         << "5. Edit KC;\n" << "6. Save;\n" 
+         << "7. Download;\n" << "0. Exit\n";
     cout << "Enter the number\n";
-    int number, number_pipes, number_kc;
-    int edit_objects;
-    string name_pipe = "", name_kc, remont;
-    int diametr_pipe, lenght_pipe;
-    string name_kc, effections;
-    int count_uex, count_uex_work;
-    //создание списка с объектами
-    list<string>objects{};
-    do {
-        cin >> number;
-        if (number == 1) {
-            cout << "Add pipes\n";
-            objects.push_back( "Pipes" ); //добавление элементов в список
-        }
-        else if (number == 2) {
-            cout << "Add KC\n";
-            objects.push_back( "KC" );
-        }
-        else if (number == 3) {
-            cout << "All objects\n";
-            if (objects.size() != 0) {
-                for (string n : objects)
-                    std::cout << n << "\n";
-            }
-            else {
-                cout << "No objects\n";
-            }
-        }
-        else if (number == 4) {
-            cout << "Edit pipes\n";
-            edit_objects = 1;
-            cout << "Enter the number of the characteristic you want to change\n";
-            cout << "1";
-           
-        }
-        else if (number == 5) {
-            cout << "Edit KC\n";
-            edit_objects = 2;
-        }
-        else if (number == 6) {
-            cout << "Save\n";
-        }
-        else if (number == 7) {
-            cout << "Download\n";
-        }
-        else if (number >= 8) {
-            cout << "Enter another command number\n";
-        }
-    } while (number != 0);
-    cout << "End\n";
+    cin >> number;
+    switch (number)
+    {
+    case 1:
+        cout << "Add pipes\n";
+        break;
+    case 2:
+        cout << "Add KC\n";
+        break;
+    case 3:
+        cout << "All objects\n";
+        break;
+    case 4:
+        cout << "Edit pipes\n";
+        break;
+    case 5:
+        cout << "Edit KS\n";
+        break;
+    case 6:
+        cout << "Save\n";
+        break;
+    case 7:
+        cout << "Download\n";
+        break;
+    case 0:
+        return false;
+        break;
+    default:
+        cout << "There is no such command, write the command number from the menu\n";
+    }
+
+
+
 }
