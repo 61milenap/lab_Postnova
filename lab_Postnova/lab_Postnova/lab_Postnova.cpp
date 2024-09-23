57f16f7e4c184de21c3362217f0ae6441b0a4d6a
61milenap <61milenap8678@gmail.com>
2024-09-23 22:02:51 +0300

    command 3, 4, 5

--- lab_Postnova/lab_Postnova/lab_Postnova.cpp
+++ lab_Postnova/lab_Postnova/lab_Postnova.cpp
@@ -5,60 +5,73 @@
 using namespace std;
 
 struct Pipe {
-    char mark_kilometr = '0km';
-    float length = 0.0, diameter = 0.0, repair = 0.0;
+    string mark_kilometr, repair;
+    float length, diameter;
 };
 
 struct CS {
-    char name='name';
-    int count_shop = 0, count_workshop = 0;
-    char perfomance = 'ok';
+    string name, perfomance;
+    int count_shop, count_workshop;
 };
 
 Pipe add_pipe()
 {
-    Pipe new_pipes;
+    Pipe new_pipe;
     cout << "Enter the pipe name in the 'numberkm' format\n";
-    cin >> new_pipes.mark_kilometr;
+    cin >> new_pipe.mark_kilometr;
     cout << "Enter the length of the pipe\n";
-    cin >> new_pipes.length;
+    cin >> new_pipe.length;
     cout << "Enter the diameter of the pipe\n";
-    cin >> new_pipes.diameter;
-    cout << "Enter the repair of the pipe\n";
-    cin >> new_pipes.repair;
-    return new_pipes;
+    cin >> new_pipe.diameter;
+    cout << "Enter the repair of the pipe in the %\n";
+    cin >> new_pipe.repair;
+    return new_pipe;
 }
 
-void input_pipe(Pipe new_pipes)
+void input_pipe(Pipe new_pipe)
 {
     cout << "information pipes\n";
-    cout << "pipe name" << " " << new_pipes.mark_kilometr << '\n';
-    cout << "pipe length" << " " << new_pipes.length << '\n';
-    cout << "pipe diameter" << " " << new_pipes.diameter << '\n';
-    cout << "pipe repair" << " " << new_pipes.repair << '\n';
+    cout << "1. pipe name" << " " << new_pipe.mark_kilometr << '\n';
+    cout << "2. pipe length" << " " << new_pipe.length << '\n';
+    cout << "3. pipe diameter" << " " << new_pipe.diameter << '\n';
+    cout << "4. pipe repair" << " " << new_pipe.repair << '\n';
 }
 
-void add_cs()
+void edit_pipe(Pipe new_pipes)
+{
+    cout << "Enter the pipe attribute that you want to edit\n";
+
+}
+
+CS add_cs()
 {
     CS new_station;
     cout << "Enter the  name of the compression station\n";
+    cin >> new_station.name;
     cout << "Enter the count shop of the compression station\n";
+    cin >> new_station.count_shop;
     cout << "Enter the count workshop of the compression station\n";
+    cin >> new_station.count_workshop;
     cout << "Enter the perfomance compression station in the format ok, good, bad\n"; 
+    cin >> new_station.perfomance;
+    return new_station;
 }
 
-void correct_input;
-
-void edit_pipe;
-
-void edit_cs;
+void input_cs(CS new_station)
+{
+    cout << "information station\n";
+    cout << "1. cs name" << " " << new_station.name << '\n';
+    cout << "2. cs count shop" << " " << new_station.count_shop << '\n';
+    cout << "3. cs count workshop" << " " << new_station.count_workshop << '\n';
+    cout << "4. cs perfomance" << " " << new_station.perfomance << '\n';
+}
 
 
 int main()
 {
     Pipe new_pipe;
-    CS new_cs;
-    int number;
+    CS new_station;
+    int number, attribute_pipe, attribute_cs;
     while (true) {
         //меню приложения
         cout << "Menu:\n";
@@ -77,17 +90,64 @@ int main()
             break;
         case 2:
             cout << "Add KC\n";
+            new_station = add_cs();
+            input_cs(new_station);
             break;
         case 3:
             cout << "All objects\n";
             input_pipe(new_pipe);
+            input_cs(new_station);
             break;
         case 4:
-            break;
+            cout << "Edit pipe\n";
+            input_pipe(new_pipe);
+            cout << "enter attribute edit\n";
+            cin >> attribute_pipe;
+            switch (attribute_pipe) {
+            case 1:
+                cout << "Enter the pipe name in the 'numberkm' format\n";
+                cin >> new_pipe.mark_kilometr;
+                break;
+            case 2:
+                cout << "Enter the length of the pipe\n";
+                cin >> new_pipe.length;
+                break;
+            case 3:
+                cout << "Enter the diameter of the pipe\n";
+                cin >> new_pipe.diameter;
+                break;
+            case 4:
+                cout << "Enter the repair of the pipe in the %\n";
+                cin >> new_pipe.repair;
+                break;
+            default:
+                cout << "Enter begin number number attribute 1, 2, 3, 4\n";
+            }
         case 5:
             cout << "Edit KS\n";
-            cout << "Enter the pipe attribute that you want to edit\n";
-            break;
+            input_cs(new_station);
+            cout << "enter attribute edit\n";
+            cin >> attribute_cs;
+            switch (attribute_cs) {
+            case 1:
+                cout << "Enter the  name of the compression station\n";
+                cin >> new_station.name;
+                break;
+            case 2:
+                cout << "Enter the count shop of the compression station\n";
+                cin >> new_station.count_shop;
+                break;
+            case 3:
+                cout << "Enter the count workshop of the compression station\n";
+                cin >> new_station.count_workshop;
+                break;
+            case 4:
+                cout << "Enter the perfomance compression station in the format ok, good, bad\n";
+                cin >> new_station.perfomance;
+                break;
+            default:
+                cout << "Enter begin number number attribute 1, 2, 3, 4\n";
+            }
         case 6:
             cout << "Save\n";
             break;
