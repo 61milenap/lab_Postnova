0100ae0128d571def2ee5a2f0fe5089f818b51de
61milenap <61milenap8678@gmail.com>
2024-12-12 19:40:20 +0300

    main new

--- lab_Postnova/lab_Postnova/lab_Postnova.cpp
+++ lab_Postnova/lab_Postnova/lab_Postnova.cpp
@@ -1,272 +1,198 @@
 #include <iostream>
-#include <limits>
-#include <fstream>
 #include <string>
+#include <fstream>
+#include "PIPEclass.h"
+#include "CSclass.h"
+#include "SaveLoad.h"
+#include "Functions.h"
+#include <unordered_map>
+#include <unordered_set>
+#include <chrono>
+#include <format>
 
-using namespace std;
-
-
-struct Pipe {
-    string mark_kilometr = "no";
-    bool repair = 0;
-    int diameter = 0;
-    double length = 0.00;
-};
-
-
-struct CS {
-    string name = "no";
-    int count_shop = 0, count_workshop = 0;
-    double perfomance = 0.00;
-};
-
-
-template <typename T>
-T GetCorrectNumber(T min, T max) {
-    T x;
-    while ((cin >> x).fail() || cin.peek() != '\n' || x < min || x > max)
-    {
-        cin.clear();
-        cin.ignore(100000, '\n');
-        cout << "\nEnter an number ("<< min << " - " << max <<") \n";
-    }
-    return x;
+void PrintMainMenu() {
+	std::cout <<
+		"1. Add a pipe\n"
+		"2. Add a compressor station\n"
+		"3. View all objects\n"
+		"4. Save\n"
+		"5. Load\n"
+		"6. Search\n"
+		"0. Exit\n\n";
 }
 
-
-bool correct_bool()
-{
-    bool bool_data;
-    while ((cin >> bool_data).fail() || cin.peek() != '\n')
-    {
-        cin.clear();
-        cin.ignore(100000, '\n');
-        cout << "\nEnter a boolean data type(0 or 1)\n";
-    }
-    return bool_data;
+void Printaddmenu() {
+	std::cout <<
+		"1. Search pipe by name\n"
+		"2. Search pipe by state\n"
+		"3. Search compressor station by name\n"
+		"4. Search compressor station by workshops\n"
+		"0. Return to main menu\n\n";
 }
 
-
-Pipe add_pipe()
-{
-    Pipe new_pipe;
-    cout << "Enter the pipe name\n";
-    getline(cin >> ws, new_pipe.mark_kilometr);
-    cout << "Enter the length of the pipe\n";
-    new_pipe.length = GetCorrectNumber(1.00, 20.00);
-    cout << "Enter the diameter of the pipe\n";
-    new_pipe.diameter = GetCorrectNumber(10, 200);
-    cout << "Enter the repair of the pipe reparing = 1 or unreparing=0\n";
-    new_pipe.repair = correct_bool();
-    return new_pipe;
+void PrintChois() {
+	std::cout << "1.Change" << std::endl << "2.Delete" << std::endl;
 }
 
-
-void output_pipe(Pipe new_pipe)
-{
-    if (new_pipe.mark_kilometr == "no") {
-        cout << "No new pipe\n";
-    }
-    else {
-        cout << "information pipes\n";
-        cout << "pipe name" << " " << new_pipe.mark_kilometr << '\n';
-        cout << "pipe length" << " " << new_pipe.length << '\n';
-        cout << "pipe diameter" << " " << new_pipe.diameter << '\n';
-        cout << "pipe repair" << " " << new_pipe.repair << '\n';
-    } 
+void savepipe(std::ofstream& fout, std::unordered_map<int, Pipe>& Pipemap) {
+	for (auto& p : Pipemap) {
+		fout << p.second;
+	}
 }
 
-
-CS add_cs()
-{
-    CS new_station;
-    string name;
-    int count_workshop;
-    bool fl_cs = false;
-    cout << "Enter the  name of the compression station\n";
-    getline(cin >> ws, new_station.name);
-    cout << "Enter the count shop of the compression station\n";
-    new_station.count_shop = GetCorrectNumber(0, 10000);
-    cout << "Enter the count workshop of the compression station\n";
-    do {
-        count_workshop = GetCorrectNumber(0, 10000);
-        if (count_workshop <= new_station.count_shop) {
-            fl_cs = true;
-        }
-        else {
-            cout << "Enter count workshop less count shop\n";
-        }
-    } while (fl_cs != true);
-    new_station.count_workshop = count_workshop;
-    cout << "Enter the perfomance compression station\n"; 
-    new_station.perfomance = GetCorrectNumber(0.00, 1.00);
-    return new_station;
+void savecs(std::ofstream& fout, std::unordered_map<int, CS>& KSmap) {
+	for (auto& g : KSmap) {
+		fout << g.second;
+	}
 }
 
-
-void output_cs(CS new_station)
-{
-    if (new_station.name == "no") {
-        cout << "No new station\n";
-    }
-    else {
-        cout << "information station\n";
-        cout << "cs name" << " " << new_station.name << '\n';
-        cout << "cs count shop" << " " << new_station.count_shop << '\n';
-        cout << "cs count workshop" << " " << new_station.count_workshop << '\n';
-        cout << "cs perfomance" << " " << new_station.perfomance << '\n';
-    }
-}
-
-
-void Save_Pipe(ofstream& fout, const Pipe& pipe) {
-    if (pipe.mark_kilometr != "no") {
-        fout << "information of pipe\n";
-        fout << pipe.mark_kilometr << '\n';
-        fout << pipe.length << '\n';
-        fout << pipe.diameter << '\n';
-        fout << pipe.repair << '\n';
-    }
-}
-
-
-void Save_Cs(ofstream& fout, const CS& cs) {
-    if (cs.name != "no") {
-        fout << "information of cs\n";
-        fout << cs.name << '\n';
-        fout << cs.count_shop << '\n';
-        fout << cs.count_workshop << '\n';
-        fout << cs.perfomance << '\n';
-    }
-}
-
-
-void Save(const Pipe& pipe, const CS& cs) {
-    ofstream fout("information.txt");
-    Save_Pipe(fout, pipe);
-    Save_Cs(fout, cs);
-    fout.close();
-}
-
-
-void Load_Pipe(ifstream& fin, Pipe& pipe) {
-    getline(fin, pipe.mark_kilometr);
-    fin >> pipe.length;
-    fin >> pipe.diameter;
-    fin >> pipe.repair;
-}
-
-
-void Load_Cs(ifstream& fin, CS& cs) {
-    getline(fin, cs.name);
-    fin >> cs.count_shop;
-    fin >> cs.count_workshop;
-    fin >> cs.perfomance;
-}
-
-
-void Load(Pipe& pipe, CS& cs) {
-    ifstream fin("information.txt");
-    string line;
-    int flag = 0;
-    if (fin.is_open()) {
-        while (getline(fin, line)) {
-            if (line == "information of pipe") {
-                Load_Pipe(fin, pipe);
-                cout << "Load pipe in structure\n";
-                flag = 1;
-            }
-            else if (line == "information of cs") {
-                Load_Cs(fin, cs);
-                cout << "Load cs in structure\n";
-                flag = 1;
-            }
-        if (flag == 0) cout << "\nNo information in file\n";
-        }
-        fin.close();
-    }   
-}
-
-
-void PrintMenu()
-{
-    cout << "Menu:\n"
-        << "1. Add pipes;\n" << "2. Add CS;\n"
-        << "3. All objects;\n" << "4. Edit pipes;\n"
-        << "5. Edit CS;\n" << "6. Save;\n"
-        << "7. Load\n" << "0. Exit\n"
-        << "Choose action: ";
-}
-
-
-int main()
-{
-    Pipe new_pipe;
-    CS new_station;
-    bool fl_cs = false;
-    while (true) {
-        PrintMenu();
-        switch (GetCorrectNumber(0, 7))
-        {
-        case 1:
-            cout << "Add pipes\n";
-            new_pipe = add_pipe();
-            output_pipe(new_pipe);
-            break;
-        case 2:
-            cout << "Add KC\n";
-            new_station = add_cs();
-            output_cs(new_station);
-            break;
-        case 3:
-            cout << "All objects\n";
-            output_pipe(new_pipe);
-            output_cs(new_station);
-            break;
-        case 4:
-            cout << "Edit pipe\n";
-            if (new_pipe.mark_kilometr == "no") {
-                cout << "No new pipe\n";
-            }
-            else {
-                cout << "editing the repair attribute for a pipe\n";
-                new_pipe.repair = correct_bool();
-            }
-            break;
-        case 5:
-            int count_workshop;
-            cout << "Edit CS\n";
-            if (new_station.name == "no") {
-                cout << "No new station\n";
-            }
-            else {
-                cout << "editing the count workshop attribute for cs\n";
-                do {
-                    count_workshop = GetCorrectNumber(0, 10000);
-                    if (count_workshop < new_station.count_shop or count_workshop == new_station.count_shop) {
-                        fl_cs = true;
-                    }
-                    else {
-                        cout << "Enter count workshop less count shop\n";
-                    }
-                } while (fl_cs != true);
-                new_station.count_workshop = count_workshop;
-            }
-            break;
-        case 6:
-            cout << "Save\n";
-            Save(new_pipe, new_station);
-            break;
-        case 7:
-            cout << "Load\n";
-            Load(new_pipe, new_station);
-            break;
-        case 0:
-            cout << "End command\n";
-            return 0;
-            break;
-        default:
-            cout << "There is no such command, write the command number from the menu\n";
-        }
-    }
-}
+void MainMenu(std::unordered_map<int, Pipe>& Pipemap, std::unordered_map<int, CS>& KSmap) {
+	std::string data;
+	std::string name;
+	bool state;
+	int work;
+	int chois;
+	std::unordered_set <int> res;
+	int userChoice;
+	while (1) {
+		PrintMainMenu();
+		userChoice = GetCorrectNumber(0, 6);
+		switch (userChoice) {
+		case 1:
+			PipesCreate(Pipemap);
+			break;
+		case 2:
+			KSCreate(KSmap);
+			break;
+		case 3:
+			PipesPrint(Pipemap);
+			KSPrint(KSmap);
+			break;
+		case 4: {
+			std::ofstream fout;
+			std::cout << "Enter ";
+			INPUT_LINE(std::cin, data);
+			fout.open(data, std::ios::out);
+			if (fout.is_open()) {
+				fout << Pipemap.size() << std::endl;
+				savepipe(fout, Pipemap);
+				fout << KSmap.size() << std::endl;
+				savecs(fout, KSmap);
+				std::cout << "Saved succesfully" << std::endl;
+			}
+			else {
+				std::cout << "Failed to write to the file..." << std::endl;
+			}
+			break;
+		}
+		case 5:
+			Load(Pipemap, KSmap);
+			break;
+		case 6: {
+			int searchChoice;
+			bool exitSubMenu = false;
+			while (!exitSubMenu) {
+				Printaddmenu();
+				searchChoice = GetCorrectNumber(0, 4);
+				switch (searchChoice) {
+				case 1:
+					if (Pipemap.empty()) {
+						std::cout << "No objects" << std::endl;
+						break;
+					}
+					else {
+						std::cout << "Enter a name for the search ";
+						INPUT_LINE(std::cin, name);
+						res = FindPipeFilter(Pipemap, checknamepipe, name);
+						PrintChois();
+						chois = GetCorrectNumber(1, 2);
+						if (chois == 1) {
+							ChangePipe(Pipemap, res);
+						}
+						if (chois == 2) {
+							DeleteObjectById<Pipe>(Pipemap, res);
+						}
+					}
+					break;
+				case 2:
+					if (Pipemap.empty()) {
+						std::cout << "No objects" << std::endl;
+						break;
+					}
+					else {
+						std::cout << "Enter a state for the search";
+						state = GetCorrectNumber(0, 1);
+						res = FindPipeFilter(Pipemap, checkstate, state);
+						PrintChois();
+						продолжи
+				case 2:
+					if (Pipemap.empty()) {
+						std::cout << "No objects" << std::endl;
+						break;
+					}
+					else {
+						std::cout << "Enter a state for the search";
+						state = GetCorrectNumber(0, 1);
+						res = FindPipeFilter(Pipemap, checkstate, state);
+						PrintChois();
+						chois = GetCorrectNumber(1, 2);
+						if (chois == 1) {
+							ChangePipe(Pipemap, res);
+						}
+						if (chois == 2) {
+							DeleteObjectById<Pipe>(Pipemap, res);
+						}
+					}
+					break;
+				case 3:
+					if (KSmap.empty()) {
+						std::cout << "No objects" << std::endl;
+						break;
+					}
+					else {
+						std::cout << "Enter a name for the search ";
+						INPUT_LINE(std::cin, name);
+						res = FindKSFilter(KSmap, checknameks, name);
+						PrintChois();
+						chois = GetCorrectNumber(1, 2);
+						if (chois == 1) {
+							ChangeKS(KSmap, res);
+						}
+						if (chois == 2) {
+							DeleteObjectById<CS>(KSmap, res);
+						}
+					}
+					break;
+				case 4:
+					if (KSmap.empty()) {
+						std::cout << "No objects" << std::endl;
+						break;
+					}
+					else {
+						std::cout << "Enter CS for search";
+						work = GetCorrectNumber(0, 100);
+						res = FindKSFilter(KSmap, workshops, work);
+						PrintChois();
+						chois = GetCorrectNumber(1, 2);
+						if (chois == 1) {
+							ChangeKS(KSmap, res);
+						}
+						if (chois == 2) {
+							DeleteObjectById<CS>(KSmap, res);
+						}
+					}
+					break;
+				case 0:
+					exitSubMenu = true;
+					break;
+				default:
+					std::cout << "Incorrect input. Try again.\n";
+					break;
+					}
+				}
+			}
+		case 0:
+			return;
+		}
+		}
+	}
